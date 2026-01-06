d064fd898bf10c8d70423362212d8e74341c84b1
thaisc98 <thaisc98@hotmail.es>
2026-01-06 09:45:05 -0400

    [fix] separating login and register components

--- src/app/user/auth-modal/auth-modal.ts
+++ src/app/user/auth-modal/auth-modal.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { Modal } from '../../shared/modal/modal';
 import { TabsContainer } from '../../shared/tabs-container/tabs-container';
 import { Tab } from '../../shared/tab/tab';
+import { Login } from '../login/login';
+import { Register } from '../register/register';
 
 @Component({
   selector: 'app-auth-modal',
-  imports: [Modal, TabsContainer, Tab],
+  imports: [Modal, TabsContainer, Tab, Login, Register],
   templateUrl: './auth-modal.html',
   styleUrl: './auth-modal.css',
 })
