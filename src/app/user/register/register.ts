3138267355a2c2767c128daaa4df3fadd09cb1f2
thaisc98 <thaisc98@hotmail.es>
2026-01-07 23:30:10 -0400

    add new register form

--- src/app/user/register/register.ts
+++ src/app/user/register/register.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, inject} from '@angular/core';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  formBuilder = inject(FormBuilder);
 
+  form = this.formBuilder.nonNullable.group({
+
+  })
 }
