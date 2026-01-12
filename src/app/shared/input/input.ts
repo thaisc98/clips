ce3c2c3acba468b2241618223b0ca0242af23724
thaisc98 <thaisc98@hotmail.es>
2026-01-12 18:20:19 -0400

    reasuable component

---
+++ src/app/shared/input/input.ts
@@ -0,0 +1,12 @@
+import { Component,input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  imports: [ReactiveFormsModule],
+  templateUrl: './input.html',
+  styleUrl: './input.css',
+})
+export class Input {
+  control = input.required<FormControl>();
+}
--- src/app/user/register/register.ts
+++ src/app/user/register/register.ts
@@ -1,9 +1,10 @@
 import { Component, inject} from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Input } from '../../shared/input/input';
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, Input],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
@@ -11,7 +12,7 @@ export class Register {
   formBuilder = inject(FormBuilder);
 
   form = this.formBuilder.nonNullable.group({
-    name: ['', [Validators.required]],
+    name: ['', [Validators.required, Validators.minLength(3)]],
     email: [''],
     age: [26],
     password: [''],
