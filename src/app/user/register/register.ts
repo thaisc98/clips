1b6f41c0553004e639602a7dee466f0b93987098
thaisc98 <thaisc98@hotmail.es>
2026-01-19 23:13:43 -0400

    validating password with patterns

--- src/app/user/register/register.ts
+++ src/app/user/register/register.ts
@@ -1,4 +1,4 @@
-import { Component, inject} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Input } from '../../shared/input/input';
@@ -15,8 +15,14 @@ export class Register {
     name: ['', [Validators.required, Validators.minLength(3)]],
     email: ['', [Validators.required, Validators.email]],
     age: [26, [Validators.required, Validators.min(18), Validators.max(90)]],
-    password: [''],
-    confirmPassword: [''],
-    phoneNumber: ['']
-  })
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/),
+      ],
+    ],
+    confirmPassword: ['', [Validators.required]],
+    phoneNumber: [''],
+  });
 }
