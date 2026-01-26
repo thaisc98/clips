da36e9b580092228bcea8fe90d293997f15a172c
thaisc98 <thaisc98@hotmail.es>
2026-01-25 22:08:35 -0400

    template form

--- src/app/user/login/login.ts
+++ src/app/user/login/login.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-
+  credentials = {
+    email: '',
+    password: '',
+  };
 }
